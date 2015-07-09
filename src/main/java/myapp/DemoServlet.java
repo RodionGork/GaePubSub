f28026ce58cfeb9f1795affa84d173b3b8569691
Rodion Gorkovenko <RodionGork@github.com>
2015-07-09 11:02:45 +0300

    Dealing with subscriptions and receiving messages

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -7,10 +7,16 @@ import java.util.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.services.pubsub.Pubsub;
+import com.google.api.services.pubsub.model.AcknowledgeRequest;
+import com.google.api.services.pubsub.model.ListSubscriptionsResponse;
 import com.google.api.services.pubsub.model.ListTopicsResponse;
 import com.google.api.services.pubsub.model.PublishRequest;
 import com.google.api.services.pubsub.model.PublishResponse;
 import com.google.api.services.pubsub.model.PubsubMessage;
+import com.google.api.services.pubsub.model.PullRequest;
+import com.google.api.services.pubsub.model.PullResponse;
+import com.google.api.services.pubsub.model.ReceivedMessage;
+import com.google.api.services.pubsub.model.Subscription;
 import com.google.api.services.pubsub.model.Topic;
 import com.google.appengine.api.utils.SystemProperty;
 
@@ -21,6 +27,8 @@ public class DemoServlet extends HttpServlet {
     private static Pubsub pubsub;
 
     private ObjectMapper mapper = new ObjectMapper();
+
+    private static Map<String, Object> OK_RESPONSE = Collections.singletonMap("ok", (Object) true);
 
     public Pubsub getPubsub() {
         if (pubsub == null) {
@@ -44,42 +52,107 @@ public class DemoServlet extends HttpServlet {
         }
         return res;
     }
-    
+
+    private List<String> listSubscriptions() throws IOException {
+        ListSubscriptionsResponse resp = getPubsub().projects().subscriptions().list("projects/" + PROJECT_ID).execute();
+        if (resp.getSubscriptions() == null) {
+            return Collections.emptyList();
+        }
+        List<String> res = new ArrayList<>();
+        for (Subscription subscription : resp.getSubscriptions()) {
+            res.add(subscription.getName().replaceFirst(".*\\/subscriptions\\/", ""));
+        }
+        return res;
+    }
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        resp.setContentType("application/json");
         Map<String, ?> result;
-        if ("topics".equals(req.getParameter("q"))) {
+        String q = req.getParameter("q");
+        String name = req.getParameter("name");
+        if ("topics".equals(q)) {
             result = Collections.singletonMap("topics", listTopics());
-        } else if ("topic".equals(req.getParameter("q"))) {
-            result = Collections.singletonMap("ok", createTopic(req.getParameter("name")));
-        } else if ("topic-del".equals(req.getParameter("q"))) {
-            result = Collections.singletonMap("ok", deleteTopic(req.getParameter("name")));
+        } else if ("topic".equals(q)) {
+            result = createTopic(name);
+        } else if ("topic-del".equals(q)) {
+            result = deleteTopic(name);
+        } else if ("subs".equals(q)) {
+            result = Collections.singletonMap("subs", listSubscriptions());
+        } else if ("sub".equals(q)) {
+            result = createSubscription(name, req.getParameter("for"));
+        } else if ("sub-del".equals(q)) {
+            result = deleteSubscription(name);
+        } else if ("fetch".equals(q)) {
+            resp.setContentType("text/plain");
+            String data = fetchData(name);
+            resp.getWriter().println(data);
+            return;
         } else {
             result = Collections.singletonMap("name", "World");
         }
+        resp.setContentType("application/json");
         resp.getWriter().println(mapper.writeValueAsString(result));
     }
 
-    private boolean deleteTopic(String name) {
-        try {
-            getPubsub().projects().topics()
-                .delete(fullTopicName(name)).execute();
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
-
-    private boolean createTopic(String name) {
+    private Map<String, Object> createTopic(String name) {
         try {
             getPubsub().projects().topics()
                     .create(fullTopicName(name), new Topic()).execute();
         } catch (Exception e) {
-            return false;
+            return errorResponse(e);
         }
-        return true;
+        return OK_RESPONSE;
+    }
+
+    private Map<String, Object> deleteTopic(String name) {
+        try {
+            getPubsub().projects().topics().delete(fullTopicName(name)).execute();
+        } catch (Exception e) {
+            return errorResponse(e);
+        }
+        return OK_RESPONSE;
+    }
+
+    private Map<String, Object> createSubscription(String name, String topic) {
+        try {
+            Subscription sub = new Subscription().setTopic(fullTopicName(topic)).setAckDeadlineSeconds(15);
+            getPubsub().projects().subscriptions().create(fullSubscriptionName(name), sub).execute();
+        } catch (Exception e) {
+            return errorResponse(e);
+        }
+        return OK_RESPONSE;
+    }
+
+    private Map<String, Object> deleteSubscription(String name) {
+        try {
+            getPubsub().projects().subscriptions().delete(fullSubscriptionName(name)).execute();
+        } catch (Exception e) {
+            return errorResponse(e);
+        }
+        return OK_RESPONSE;
+    }
+
+    private String fetchData(String name) {
+        name = fullSubscriptionName(name);
+        PullRequest req = new PullRequest().setReturnImmediately(false).setMaxMessages(1);
+        PullResponse resp;
+        try {
+            resp = getPubsub().projects().subscriptions().pull(name, req).execute();
+        } catch (Exception e) {
+            return "Nothing was received... :(";
+        }
+        List<ReceivedMessage> msgs = resp.getReceivedMessages();
+        if (msgs == null || msgs.isEmpty()) {
+            return "Empty response for unknown reason... :(";
+        }
+        AcknowledgeRequest ack = new AcknowledgeRequest().setAckIds(Arrays.asList(msgs.get(0).getAckId()));
+        try {
+            getPubsub().projects().subscriptions().acknowledge(name, ack).execute();
+        } catch (Exception e) {
+            return "Error while sending acknowledgement :(";
+        }
+        return new String(msgs.get(0).getMessage().decodeData());
     }
 
     @Override
@@ -103,4 +176,17 @@ public class DemoServlet extends HttpServlet {
     private String fullTopicName(String name) {
         return "projects/" + PROJECT_ID + "/topics/" + name;
     }
+
+    private String fullSubscriptionName(String name) {
+        return "projects/" + PROJECT_ID + "/subscriptions/" + name;
+    }
+
+    private Map<String,Object> errorResponse(Exception e) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("ok", false);
+        map.put("error", e.toString());
+        map.put("message", e.getMessage());
+        return map;
+    }
+
 }
--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     updateTopicList();
+    updateSubsList();
 });
 
 function updateTopicList() {
@@ -13,10 +14,37 @@ function updateTopicList() {
     });
 }
 
+function updateSubsList() {
+    $.getJSON('/demo?q=subs', function(data) {
+        var select = $('#subs');
+        select.empty();
+        for (var i in data.subs) {
+            var name = data.subs[i];
+            $('<option>' + name + '</option>').attr('value', name).appendTo(select);
+        }
+    });
+}
+
 function publishData() {
     var topic = $('#topics').val();
     var data = $('#data').val();
     $.post('/demo?topic=' + topic, data, function() {alert('Ok!')}, 'text');
+}
+
+function fetchData() {
+    var sub = $('#subs').val();
+    var receiver = $('#receiver');
+    receiver.text('Please wait a few seconds...');
+    receiver.load('/demo?q=fetch&name=' + sub);
+}
+
+function reportResult(data) {
+    if (data.ok) {
+        alert("SUCCESS!");
+    } else {
+        alert("ERROR:\n\n" + data.error + "\n\n" + data.message);
+    }
+
 }
 
 function createTopic() {
@@ -25,7 +53,7 @@ function createTopic() {
         return;
     }
     $.getJSON('/demo?q=topic&name=' + name, function(data) {
-        alert(data.ok ? "Topic was created!" : "Some error had happened");
+        reportResult(data);
         updateTopicList();
     });
 }
@@ -37,7 +65,31 @@ function deleteTopic() {
         return;
     }
     $.getJSON('/demo?q=topic-del&name=' + topic, function(data) {
-        alert(data.ok ? "Topic was deleted!" : "Some error had happened");
+        reportResult(data);
         updateTopicList();
+    });
+}
+
+function createSub() {
+    var topic = $('#topics').val();
+    var name = prompt("New Subscription Name (for topic '" + topic + "')");
+    if (name == null) {
+        return;
+    }
+    $.getJSON('/demo?q=sub&name=' + name + '&for=' + topic, function (data) {
+        reportResult(data);
+        updateSubsList();
+    });
+}
+
+function deleteSub() {
+    var name = $('#subs').val();
+    var ur = confirm("Delete subscription '" + name + "' - are you sure?");
+    if (!ur) {
+        return;
+    }
+    $.getJSON('/demo?q=sub-del&name=' + name, function(data) {
+        reportResult(data);
+        updateSubsList();
     });
 }
